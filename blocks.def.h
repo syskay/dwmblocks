fdc36989cd1091ae5f01010247185d94defcc945
syskay <syskay@gmail.com>
2021-03-03 10:53:41 +0100

    blocks.def.h config update

--- blocks.def.h
+++ blocks.def.h
@@ -1,12 +1,13 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	{"Mem:", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",	30,		0},
-
-	{"",		"battery",		60,			0},
-	{"",		"kernel",		3600,			0},
+	//{"Mem:", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",	30,		0},
+	
 	{"",		"weather",		60,			0},
+	{"",		"kernel",		3600,			0},
 	{"",		"system",		60,			0},
+	{"",		"disk",			60,			0},
+	{"",		"battery",		60,			0},
 	{"",		"clock",		30,			0},
 };
 
