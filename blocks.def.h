88400c8773198a829ae1d755f51b8b36efe53407
syskay <syskay@gmail.com>
2021-05-06 08:59:28 +0200

    status bar scripts name update

--- blocks.def.h
+++ blocks.def.h
@@ -3,12 +3,14 @@ static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
 	//{"Mem:", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",	30,		0},
 	
-	{"",		"weather",		60,			1},
-	{"",		"kernel",		3600,			2},
-	{"",		"system",		60,			3},
-	{"",		"disk",			60,			4},
-	{"",		"battery",		60,			5},
-	{"",		"clock",		30,			6},
+	{"",			"weather-sb",			60,				1},
+	{"",			"kernel-sb",			3600,			2},
+	{"",			"memory-sb",			1,				3},
+	{"",			"system-sb",			60,				4},
+	{"",			"disk-sb",				60,				5},
+	{"",			"battery-sb",			60,				6},
+	{"",			"clock-sb",				30,				7},
+	{"", 	  	"user-sb",				60,				8},
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
